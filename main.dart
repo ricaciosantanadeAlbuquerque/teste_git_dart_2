2102f805a33424a92a72d04ccc9f8075fc6f432c
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-09-16 21:13:05 -0300

    correção

--- main.dart
+++ main.dart
@@ -73,5 +73,7 @@ void subtracao(String valor1, String valor2) {
   int num1 = int.parse(valor1);
   int num2 = int.parse(valor2);
 
-  print("Soma ${num1 - num2}");
+  print("Subtração ${num1 - num2}");
 }
+
+
